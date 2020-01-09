3f59141dbeba94098db95117bdeb2df38d313d15
hfolk25 <hfolk25@gmail.com>
2020-01-08 18:26:06 -0800

    changing route name for get method

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/index", function(req, res) {
+router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     // console.log(data);
     res.render("index", { burgers: data });
