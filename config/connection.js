8d4e22c9fd26544b8d9de70aa3003826b3d48f4f
hfolk25 <hfolk25@gmail.com>
2021-02-08 10:32:01 -0800

    deleting unnecessary comments and console logs and prettier-fying everything

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,3 @@
-// Set up MySQL connection.
 const mysql = require("mysql");
 let connection;
 
@@ -6,16 +5,15 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db",
   });
-};
+}
 
-// Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -23,5 +21,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
